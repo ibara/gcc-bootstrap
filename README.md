47d357b3d2b85acdd9e6acea0e203cdd767bdc62
Brian Callahan <bcallah@openbsd.org>
2025-08-11 14:54:24 -0400

    Initial commit of gcc-bootstrap

---
+++ README.md
@@ -0,0 +1,40 @@
+gcc-bootstrap
+=============
+A simple to use shell script to bootstrap niche GCC languages on your
+machine, such as
+[D](https://dlang.org/).
+
+Usage
+-----
+```sh
+$ ./gcc-bootstrap.sh [options] gcc_ver installdir
+```
+Where `gcc_ver` is the version number you want to build (e.g., 15.2.0)
+and `installdir` is the directory you want to install GCC into.
+
+The options are as follows:
+```
+  --bootstrap    Download and use bootstrap binaries
+  --full         Build all possible GCC languages for your target
+  --with-ada     Add Ada to the list of languages to build
+  --with-cobol   Add COBOL to the list of languages to build
+  --with-d       Add D to the list of languages to build
+  --with-fortran Add Fortran to the list of languages to build
+  --with-go      Add Go to the list of languages to build
+  --with-m2      Add Modula-2 to the list of languages to build
+  --with-objc    Add Objective C to the list of languages to build
+                 Includes adding Objective C++
+```
+All other flags that begin with `--` will be passed to the GCC
+configure script.
+
+For example, to build GDC 15.1.0 on macOS when you don't already have
+GDC installed on your system, and you want to install it to
+`/opt/gcc`, you would run:
+```sh
+$ ./gcc-bootstrap.sh --bootstrap --with-d 15.1.0 /opt/gcc
+```
+
+License
+-------
+ISC License. See `LICENSE` for more information.
---
+++ gcc-bootstrap.sh
@@ -0,0 +1,344 @@
+#!/bin/sh
+
+usage="usage: build-gcc.sh [options] gcc_ver installdir"
+
+uname_m="$(uname -m)"
+uname_r="$(uname -r)"
+uname_s="$(uname -s)"
+
+macos_amd64_bootstrap=1
+macos_arm64_bootstrap=2
+macos_arm64=0
+
+bootstrap=0
+languages="c,c++"
+full=0
+
+fatal() {
+  printf "error: %s\n" "$1"
+  exit 1
+}
+
+check_bootstrap() {
+  printf "SHA256 check... "
+
+  check="$(echo "$1" | awk '{print $1}')"
+
+  case "$uname_s" in
+    Darwin)
+      case "$gcc_ver" in
+        "15.1.0")
+          hash="02758c6b60b6037633dc3b6950dabb6d8dd83f32e03a463010be76eb5f004c1a"
+          ;;
+      esac
+      ;;
+  esac
+
+  if [ "$check" != "$hash" ] ; then
+    fatal "Hash mismatch"
+  fi
+
+  echo "ok"
+}
+
+check_sha256() {
+  printf "SHA256 check... "
+
+  check="$(echo "$1" | awk '{print $1}')"
+
+  case "$gcc_ver" in
+    "15.1.0")
+      hash="51b9919ea69c980d7a381db95d4be27edf73b21254eb13d752a08003b4d013b1"
+      ;;
+    "15.2.0")
+      hash="7294d65cc1a0558cb815af0ca8c7763d86f7a31199794ede3f630c0d1b0a5723"
+      ;;
+  esac
+
+  if [ "$check" != "$hash" ] ; then
+    fatal "Hash mismatch"
+  fi
+
+  echo "ok"
+}
+
+find_downloader() {
+  # You need curl or wget
+  command -v curl > /dev/null 2>&1
+  if [ $? -eq 0 ] ; then
+    downloader="$(command -v curl) -LO"
+  else
+    command -v wget > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+      downloader="$(command -v wget)"
+    fi
+  fi
+
+  if [ -z "$downloader" ] ; then
+    fatal "You must install one of curl or wget"
+  fi
+}
+
+setup_build() {
+  # You need GNU make 3.80 or later
+  # Non-Linux platforms often call it gmake
+  # That's almost always acceptable
+  for make_prog in "gmake" "make" ; do
+    command -v $make_prog > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+      $make_prog --version 2>&1 | grep -q "GNU Make"
+      if [ $? -ne 0 ] ; then
+        continue
+      fi
+      make_ver="$($make_prog --version)"
+      make_ver=$(echo $make_ver | awk '{print $3}')
+      make_major=$(echo $make_ver | awk -F '.' '{print $1}')
+      make_minor=$(echo $make_ver | awk -F '.' '{print $2}')
+      if [ $make_major -gt 3 ] ; then
+        make_program="$(command -v $make_prog)"
+        break
+      else
+        if [ $make_major -eq 3 ] ; then
+          if [ $make_minor -ge 80 ] ; then
+            make_program="$(command -v $make_prog)"
+            break
+          fi
+        fi
+      fi
+    fi
+  done
+
+  if [ -z "$make_program" ] ; then
+    fatal "You need GNU make"
+  fi
+
+  case "$uname_s" in
+    Darwin)
+      # Turn off Go on Darwin
+      if [ $full -eq 1 ] ; then
+        languages="ada,c,c++,cobol,d,fortran,m2,objc,obj-c++"
+      fi
+      # How many CPUs are online?
+      ncpu=$(getconf _NPROCESSORS_ONLN)
+      # Should really detect one of Xcode or CLT, but I only use CLT...
+      extra="--with-sysroot=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"
+
+      if [ "$uname_m" = "arm64" ] ; then
+        # Darwin/arm64 support not fully upstreamed to GCC yet...
+        if [ "$gcc_ver" != "15.1.0" ] ; then
+          echo "warning: Setting GCC version to 15.1.0"
+          gcc_ver="15.1.0"
+        fi
+        # We need GNU patch to apply diff
+        command -v gpatch > /dev/null 2>&1
+        if [ $? -ne 0 ] ; then
+          fatal "You need GNU patch on arm64 macOS"
+        fi
+        patch="$(command -v gpatch)"
+        macos_arm64=1
+      fi
+      ;;
+    *)
+      # No idea what we are, assume just 1 CPU to be safe.
+      ncpu=1
+      ;;
+  esac
+}
+
+get_prereqs() {
+  original="$(pwd)"
+  cd "$(pwd)/gcc-$gcc_ver"
+  ./contrib/download_prerequisites
+  if [ $? -eq 1 ] ; then
+    fatal "Failed to download gcc prerequisites"
+  fi
+  cd "$original"
+}
+
+find_bootstrap() {
+  case "$uname_s" in
+    "Darwin")
+      # We have bootstraps going back to 12.7.6 on amd64 (darwin21.6.0)
+      # We have bootstraps going back to 15.6 on arm64 (darwin24.6.0)
+      major=$(echo $uname_r | awk -F '.' '{print $1}')
+      minor=$(echo $uname_r | awk -F '.' '{print $2}')
+      if [ "$uname_m" = "x86_64" ] ; then
+        if [ $major -eq 21 ] ; then
+          minor=$(echo $uname_r | awk -F '.' '{print $2}')
+          if [ $minor -ge 6 ] ; then
+            return $macos_amd64_bootstrap
+          fi
+        elif [ $major -gt 21 ] ; then
+          return $macos_amd64_bootstrap
+        fi
+      elif [ "$uname_m" = "arm64" ] ; then
+        if [ $major -eq 24 ] ; then
+          if [ $minor -ge 6 ] ; then
+            return $macos_arm64_bootstrap
+          fi
+        elif [ $major -ge 24 ] ; then
+          return $macos_arm64_bootstrap
+        fi
+      fi
+    ;;
+  esac
+
+  fatal "Sorry, no bootstrap available. Please make a pull request."
+}
+
+for opt ; do
+  case "$opt" in
+    --bootstrap)
+      # Determine if we have a bootstrap to offer...
+      find_bootstrap
+      bootstrap=$?
+      ;;
+    --full)
+      languages="all"
+      full=1
+      ;;
+    --with-ada)
+      languages="ada,${languages}"
+      ;;
+    --with-cobol)
+      languages="${languages},cobol"
+      ;;
+    --with-d)
+      languages="${languages},d"
+      ;;
+    --with-fortran)
+      languages="${languages},fortran"
+      ;;
+    --with-go)
+      languages="${languages},fortran"
+      ;;
+    --with-m2)
+      languages="${languages},m2"
+      ;;
+    --with-objc)
+      languages="${languages},objc,obj-c++"
+      ;;
+    -h|--help)
+      echo $usage
+      exit 0
+      ;;
+    --*)
+      user_extra="$user_extra $opt"
+      ;;
+    *)
+      if [ -z "$gcc_ver" ] ; then
+        gcc_ver="$opt"
+      elif [ -z "$installdir" ] ; then
+        installdir="$opt"
+      else
+        fatal $usage
+      fi
+  esac
+done
+
+find_downloader
+
+if [ $bootstrap -gt 0 ] ; then
+  case $bootstrap in
+    1)
+      bootstrap_url=""
+      ;;
+    2)
+      bootstrap_url=""
+      ;;
+    *)
+      fatal "Unknown bootstrap"
+      ;;
+  esac
+  printf "Found bootstrap: %s\n" $bootstrap_url
+fi
+
+if [ -z $gcc_ver ] ; then
+  fatal "Must specify gcc version number"
+fi
+
+if [ -z $installdir ] ; then
+  fatal "Must specify gcc installation directory"
+fi
+
+setup_build
+
+printf "Cleaning... "
+
+rm -rf "$(pwd)/gcc-$gcc_ver"
+mkdir -p "$(pwd)/gcc-$gcc_ver"
+cd "$(pwd)/gcc-$gcc_ver"
+
+echo "done"
+
+if [ $bootstrap -ne 0 ] ; then
+  printf "Downloading bootstrap"
+  $downloader $bootstrap_url
+
+  check_sha256 "$(sha256sum $(basename $bootstrap_url))"
+
+  printf "Untarring... "
+  tar xzf $(basename $bootstrap_url)
+  echo "done"
+fi
+
+url="https://ftpmirror.gnu.org/gnu/gcc/gcc-$gcc_ver/gcc-$gcc_ver.tar.gz"
+
+printf "Downloading %s\n" $url
+$downloader $url
+
+check_sha256 "$(sha256sum $(basename $url))"
+
+printf "Untarring... "
+tar xzf gcc-$gcc_ver.tar.gz
+echo "done"
+
+if [ $macos_arm64 -eq 1 ] ; then
+  original="$(pwd)"
+  cd "$(pwd)/gcc-$gcc_ver"
+  patch_url="https://raw.githubusercontent.com/Homebrew/formula-patches/575ffcaed6d3112916fed77d271dd3799a7255c4/gcc/gcc-15.1.0.diff"
+  $downloader $patch_url
+
+  printf "SHA256 check... "
+  check="$(sha256sum $(basename $patch_url) | awk '{print $1}')"
+  hash="360fba75cd3ab840c2cd3b04207f745c418df44502298ab156db81d41edf3594"
+  if [ "$check" != "$hash" ] ; then
+    fatal "Hash mismatch"
+  fi
+  echo "ok"
+
+  patch_file="$(basename $patch_url)"
+  $patch -p1 <$patch_file
+  cd "$original"
+fi
+
+# Force consistency
+get_prereqs
+
+rm -rf "$(pwd)/build-gcc-$gcc_ver"
+mkdir -p "$(pwd)/build-gcc-$gcc_ver"
+cd "$(pwd)/build-gcc-$gcc_ver"
+
+#configure_invocation="$(pwd)/../gcc-$gcc_ver/configure --prefix=$installdir --enable-languages=$languages $extra $user_extra"
+configure_invocation="../gcc-$gcc_ver/configure --enable-languages=$languages $extra $user_extra"
+echo "$configure_invocation"
+
+#"$(pwd)/../gcc-$gcc_ver/configure" --prefix=$installdir --enable-languages=$languages $extra $user_extra
+"../gcc-$gcc_ver/configure" --enable-languages=$languages $extra $user_extra
+
+echo "$make_program V=1 -j$ncpu"
+$make_program V=1 -j$ncpu
+
+if [ $? -eq 0 ] ; then
+  echo "$make_program DESTDIR=$(pwd)/../fake-gcc-$gcc_ver install"
+  rm -rf "$(pwd)/../fake-gcc-$gcc_ver"
+  mkdir -p "$(pwd)/../fake-gcc-$gcc_ver"
+  $make_program DESTDIR=$(pwd)/../fake-gcc-$gcc_ver install
+  cd "$(pwd)/../fake-gcc-$gcc_ver"
+  base="$(ls -1)"
+  tar -cz -f gcc-$gcc_ver.tar.gz "$base"
+  #sudo tar xzf gcc-$gcc_ver.tar.gz -C /
+  printf "Installed gcc-%s to %s\n" "$gcc_ver" "$installdir"
+else
+  fatal "GCC build failed"
+fi
